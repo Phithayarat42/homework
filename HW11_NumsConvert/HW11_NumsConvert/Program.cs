2bb6ac0300ec8173e17ceb213845dee974aa526e
Phithayarat42 <76816871+Phithayarat42@users.noreply.github.com>
2021-06-30 12:44:03 +0700

    add new hw11 and hw12 files

--- HW11_NumsConvert/HW11_NumsConvert/Program.cs
+++ HW11_NumsConvert/HW11_NumsConvert/Program.cs
@@ -1,32 +1,220 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace HW11_NumsConvert
+namespace HW11
 {
     public interface IHomework11
     {
         string GetReadWordOfNumber(int number);
     }
-    class numCon : IHomework11 {
-        public int GetReadWordOfNumber(int number) {
-            string[] thNum = { "ศูนย์", "หนึ่ง", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า", "สิบ", "เอ็ด" };
-            string[] thN = { "", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน" };
+    class Numcon : IHomework11
+    {
+        public string GetReadWordOfNumber(int number)
+        {
+            string text;
+            int len = Convert.ToString(number).Length;
+            List<String> wnums = new List<string>();
 
-            if (number == 0) {
-                Console.Write("ศูนย์");
+            if (number >= 0)
+            {
+                
+                Console.WriteLine(" \n Output");
+
+                for (int i = 0; i < len; i++)
+                {
+                    text = Convert.ToString(number).Substring(i, 1);
+
+                    if (len == 1)
+                    {
+                        string[] n = { "ศููนย์", "หนึ่ง", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า" };
+                        wnums.Add(n[Convert.ToInt32(text)]);
+                        break;
+                    }
+
+                    else if (len == 2)
+                    {
+                        if (i == 0)
+                        {
+                            string[] n = { "", "สิบ", "ยี่สิบ", "สามสิบ", "สี่สิบ", "ห้าสิบ", "หกสิบ", "เจ็ดสิบ", "แปดสิบ", "เก้าสิบ" };
+                            wnums.Add(n[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 1)
+                        {
+                            string[] n1 = { "", "เอ็ด", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า" };
+                            wnums.Add(n1[Convert.ToInt32(text)]);
+                            break;
+                        }
+                    }
+                    else if (len == 3)
+                    {
+                        if (i == 0)
+                        {
+                            string[] n = { "", "หนึ่งร้อย", "สองร้อย", "สามร้อย", "สี่ร้อย", "ห้าร้อย", "หกร้อย", "เจ็ดร้อย", "แปดร้อย", "เก้าร้อย" };
+                            wnums.Add(n[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 1)
+                        {
+                            string[] n1 = { "", "สิบ", "ยี่สิบ", "สามสิบ", "สี่สิบ", "ห้าสิบ", "หกสิบ", "เจ็ดสิบ", "แปดสิบ", "เก้าสิบ" };
+                            wnums.Add(n1[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 2)
+                        {
+                            string[] n2 = { "", "เอ็ด", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า" };
+                            wnums.Add(n2[Convert.ToInt32(text)]);
+                            break;
+                        }
+                    }
+                    else if (len == 4)
+                    {
+                        if (i == 0)
+                        {
+                            string[] n = { "", "หนึ่งพัน", "สองพัน", "สามพัน", "สี่พัน", "ห้าพัน", "หกพัน", "เจ็ดพัน", "แปดพัน", "เก้าพัน" };
+                            wnums.Add(n[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 1)
+                        {
+                            string[] n1 = { "", "หนึ่งร้อย", "สองร้อย", "สามร้อย", "สี่ร้อย", "ห้าร้อย", "หกร้อย", "เจ็ดร้อย", "แปดร้อย", "เก้าร้อย" };
+                            wnums.Add(n1[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 2)
+                        {
+                            string[] n2 = { "", "สิบ", "ยี่สิบ", "สามสิบ", "สี่สิบ", "ห้าสิบ", "หกสิบ", "เจ็ดสิบ", "แปดสิบ", "เก้าสิบ" };
+                            wnums.Add(n2[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 3)
+                        {
+                            string[] n3 = { "", "เอ็ด", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า" };
+                            wnums.Add(n3[Convert.ToInt32(text)]);
+                            break;
+                        }
+
+                    }
+                    else if (len == 5)
+                    {
+                        if (i == 0)
+                        {
+                            string[] n = { "", "หนึ่งหมื่น", "สองหมื่น", "สามหมื่น", "สี่หมื่น", "ห้าหมื่น", "หกหมื่น", "เจ็ดหมื่น", "แปดหมื่น", "เก้าหมื่น" };
+                            wnums.Add(n[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 1)
+                        {
+                            string[] n1 = { "", "หนึ่งพัน", "สองพัน", "สามพัน", "สี่พัน", "ห้าพัน", "หกพัน", "เจ็ดพัน", "แปดพัน", "เก้าพัน" };
+                            wnums.Add(n1[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 2)
+                        {
+                            string[] n2 = { "", "หนึ่งร้อย", "สองร้อย", "สามร้อย", "สี่ร้อย", "ห้าร้อย", "หกร้อย", "เจ็ดร้อย", "แปดร้อย", "เก้าร้อย" };
+                            wnums.Add(n2[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 3)
+                        {
+                            string[] n3 = { "", "สิบ", "ยี่สิบ", "สามสิบ", "สี่สิบ", "ห้าสิบ", "หกสิบ", "เจ็ดสิบ", "แปดสิบ", "เก้าสิบ" };
+                            wnums.Add(n3[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 4)
+                        {
+                            string[] n4 = { "", "เอ็ด", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า" };
+                            wnums.Add(n4[Convert.ToInt32(text)]);
+                            break;
+                        }
+                    }
+                    else if (len == 6)
+                    {
+                        if (i == 0)
+                        {
+                            string[] n = { "", "หนึ่งแสน", "สองแสน", "สามแสน", "สี่แสน", "ห้าแสน", "หกแสน", "เจ็ดแสน", "แปดแสน", "เก้าแสน" };
+                            wnums.Add(n[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 1)
+                        {
+                            string[] n1 = { "", "หนึ่งหมื่น", "สองหมื่น", "สามหมื่น", "สี่หมื่น", "ห้าหมื่น", "หกหมื่น", "เจ็ดหมื่น", "แปดหมื่น", "เก้าหมื่น" };
+                            wnums.Add(n1[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 2)
+                        {
+                            string[] n2 = { "", "หนึ่งพัน", "สองพัน", "สามพัน", "สี่พัน", "ห้าพัน", "หกพัน", "เจ็ดพัน", "แปดพัน", "เก้าพัน" };
+                            wnums.Add(n2[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 3)
+                        {
+                            string[] n3 = { "", "หนึ่งร้อย", "สองร้อย", "สามร้อย", "สี่ร้อย", "ห้าร้อย", "หกร้อย", "เจ็ดร้อย", "แปดร้อย", "เก้าร้อย" };
+                            wnums.Add(n3[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 4)
+                        {
+                            string[] n4 = { "", "สิบ", "ยี่สิบ", "สามสิบ", "สี่สิบ", "ห้าสิบ", "หกสิบ", "เจ็ดสิบ", "แปดสิบ", "เก้าสิบ" };
+                            wnums.Add(n4[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 5)
+                        {
+                            string[] n5 = { "", "เอ็ด", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า" };
+                            wnums.Add(n5[Convert.ToInt32(text)]);
+                            break;
+                        }
+                    }
+                    else if (len == 7)
+                    {
+                        if (i == 0)
+                        {
+                            string[] n = { "", "หนึ่งล้าน", "สองล้าน", "สามล้าน", "สี่ล้าน", "ห้าล้าน", "หกล้าน", "เจ็ดล้าน", "แปดล้าน", "เก้าล้าน" };
+                            wnums.Add(n[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 1)
+                        {
+                            string[] n1 = { "", "หนึ่งแสน", "สองแสน", "สามแสน", "สี่แสน", "ห้าแสน", "หกแสน", "เจ็ดแสน", "แปดแสน", "เก้าแสน" };
+                            wnums.Add(n1[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 2)
+                        {
+                            string[] n2 = { "", "หนึ่งหมื่น", "สองหมื่น", "สามหมื่น", "สี่หมื่น", "ห้าหมื่น", "หกหมื่น", "เจ็ดหมื่น", "แปดหมื่น", "เก้าหมื่น" };
+                            wnums.Add(n2[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 3)
+                        {
+                            string[] n3 = { "", "หนึ่งพัน", "สองพัน", "สามพัน", "สี่พัน", "ห้าพัน", "หกพัน", "เจ็ดพัน", "แปดพัน", "เก้าพัน" };
+                            wnums.Add(n3[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 4)
+                        {
+                            string[] n4 = { "", "หนึ่งร้อย", "สองร้อย", "สามร้อย", "สี่ร้อย", "ห้าร้อย", "หกร้อย", "เจ็ดร้อย", "แปดร้อย", "เก้าร้อย" };
+                            wnums.Add(n4[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 5)
+                        {
+                            string[] n5 = { "", "สิบ", "ยี่สิบ", "สามสิบ", "สี่สิบ", "ห้าสิบ", "หกสิบ", "เจ็ดสิบ", "แปดสิบ", "เก้าสิบ" };
+                            wnums.Add(n5[Convert.ToInt32(text)]);
+                        }
+                        else if (i == 6)
+                        {
+                            string[] n6 = { "", "เอ็ด", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า" };
+                            wnums.Add(n6[Convert.ToInt32(text)]);
+                            break;
+                        }
+                    }
+                }
+
+                foreach (string i in wnums)
+                {
+                    Console.Write(i);
+                }
             }
-            
+            Console.WriteLine("");
+            return null;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Please input number: ");
-            string sents = Console.ReadLine();
+            Console.WriteLine("Please input number : ");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            Numcon x = new Numcon();
+            x.GetReadWordOfNumber(numb);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ HW12_DupNodup/HW12_DupNodup/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12_DupNodup
+{
+    public interface IHomework12
+    {
+        char FirstDuplicateCharactor(string text);
+        char FirstNotDuplicateCharactor(string text);
+    }
+    class checkChar : IHomework12
+    {
+        char txt;
+        public char FirstDuplicateCharactor(string text)
+        {
+            string c;
+
+            List<String> word = new List<string>();
+            List<String> wordlist = new List<string>();
+
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                c = text.Substring(i, 1);
+                word.Add(c);
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (word[i] == word[j] && i != j)
+                    {
+                        wordlist.Add(word[i]);
+                        i = text.Length;
+                        j = text.Length;
+                        break;
+                    }
+                }
+            }
+            if (wordlist.Count == 0)
+            {
+                Console.WriteLine("First duplicate charactor is: -");
+            }
+
+            foreach (string i in wordlist)
+            {
+                Console.WriteLine("First duplicate charactor is: " + i);
+            }
+            return txt;
+        }
+        public char FirstNotDuplicateCharactor(string text)
+        {
+            string c;
+            int len = text.Length;
+            List<String> word = new List<string>();
+            List<String> wordlist = new List<string>();
+
+
+            for (int i = 0; i < len; i++)
+            {
+                c = text.Substring(i, 1);
+                word.Add(c);
+                wordlist.Add(c);
+            }
+
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    if (word[i] == word[j] && i != j)
+                    {
+                        wordlist.Remove(word[i]);
+                        break;
+                    }
+                }
+            }
+            if (wordlist.Count == 0)
+            {
+                Console.WriteLine("First not duplicate charactor is: -");
+            }
+            foreach (string i in wordlist)
+            {
+                Console.WriteLine("First not duplicate charactor is: " + i);
+                break;
+            }
+            Console.WriteLine(" ");
+            return txt; ;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input string : ");
+            string t = Console.ReadLine();
+
+            checkChar check = new checkChar();
+            check.FirstDuplicateCharactor(t);
+            check.FirstNotDuplicateCharactor(t);
+            Console.ReadKey();
+        }
+    }
+}
